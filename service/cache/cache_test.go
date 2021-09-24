51e621e02314ce623fe2a1cde31901b9196a4228
danbai <danbai@88.com>
2021-09-24 17:45:53 +0800

    init

---
+++ service/cache/cache_test.go
@@ -0,0 +1,19 @@
+package cache
+
+import (
+	"testing"
+)
+
+func TestCache(t *testing.T) {
+	err := Cache("123", []byte("test"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cache, err := getCache("123")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(cache) != "test" {
+		t.Fatal("缓存错误")
+	}
+}
